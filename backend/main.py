ae7a5dbc69d6c455cd328f733db384267b3fa4ec
shivampandey111 <shivampandey33033@gmail.com>
2026-06-02 01:42:19 +0530

    Auth completed

---
+++ backend/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routers import auth, query, upload
+
+app = FastAPI(title="Insurer Rights")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["http://localhost:5173"],
+    allow_credentials = True,
+    allow_headers = ['*'],
+    allow_methods = ['*']
+)
+
+# Register Routes from router file
+
+app.include_router(
+    auth.router,
+    prefix='/auth',
+    tags = ['Auth']
+)
+app.include_router(
+    query.router,
+    prefix='/query',
+    tags=['Query']
+)
+app.include_router(
+    upload.router,
+    prefix='/upload',
+    tags=['Upload']
+)
+
+@app.get('/api/health')
+def health():
+    return {'status':'ok'}
---
+++ backend/routers/auth.py
@@ -0,0 +1,64 @@
+from pydantic import BaseModel, EmailStr, Field
+from fastapi import APIRouter, HTTPException
+from database import get_supabase
+from dotenv import load_dotenv
+import os
+router = APIRouter()
+load_dotenv()
+
+supabase = get_supabase()
+
+class RegisterUser(BaseModel):
+    email : EmailStr
+    password : str = Field(min_length=8)
+
+class LoginUser(BaseModel):
+    email : EmailStr
+    password : str
+
+# Register User
+
+@router.post('/register')
+async def register_user(user:RegisterUser):
+    try:
+        response = supabase.auth.sign_up({
+            "email" : user.email,
+            "password" : user.password
+        })
+        return {
+            "message" : "User Created Successfully",
+            "user_id" : response.user.id
+        }
+    except Exception as e:
+        raise HTTPException(
+            status_code=400,
+            detail=str(e)
+        )
+
+# Login User
+
+@router.post('/login')
+async def loginUser(user:LoginUser):
+    try:
+        response = supabase.auth.sign_in_with_password({
+            "email" : user.email,
+            "password" : user.password
+        })
+        return {
+            "access_token" : response.session.access_token,
+            "refresh_token" : response.session.refresh_token,
+            "user_id" : response.user.id,
+            "message" : "User Logged In Successfully"
+        }
+    except:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid Credentials"
+        )
+    
+# Logout
+@router.post('/logOut')
+async def logOut():
+    return {
+        "message" : "Remove access and refresh tokens"
+    }
