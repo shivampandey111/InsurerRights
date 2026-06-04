c388aa798b0ba124ed51212ecebd0f97feac8df4
shivampandey111 <shivampandey33033@gmail.com>
2026-06-04 11:54:31 +0530

    Current User Dependency

---
+++ backend/routers/dependencies.py
@@ -0,0 +1,29 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from backend.supaBase import get_supabase
+
+security = HTTPBearer()
+
+async def get_current_user(
+        # Extracts the JWT from auth header and verifies it from supabase and return a uuid as str
+
+        credentials : HTTPAuthorizationCredentials = Depends(security),
+        supabase = Depends(get_supabase)
+) -> str :
+    token = credentials.credentials
+
+    try:
+        response = supabase.auth.get_user(token)
+        user = response.user
+
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid or expired token"
+            )
+        return str(user.id)
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials"
+        )
