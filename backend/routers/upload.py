0df00affd9323b7b48c051aae5d1ed87bc53ef13
shivampandey111 <shivampandey33033@gmail.com>
2026-06-04 12:14:09 +0530

    Initial Upload.py

---
+++ backend/routers/upload.py
@@ -0,0 +1,52 @@
+from fastapi import APIRouter, File, UploadFile, Depends, HTTPException
+from database import get_supabase
+from services.pdf_service import extract_content_from_pdf, get_chunks
+from services.embedding_service import get_embeds
+from routers.dependencies import get_current_user
+import uuid
+router = APIRouter()
+
+@router.post('/upload')
+async def upload_pdf(
+    file: UploadFile = File(...), 
+    user_id = Depends(get_current_user)
+):
+    # Validate the type and size of the file
+    if file.content_type != "application/pdf":
+        raise HTTPException(400, "Only pdf files are expected")
+    
+    file_bytes = await file.read()
+    if len(file_bytes) > 20 * 1024 * 1024:
+        raise HTTPException(401, "Max file size allowed is 10mb")
+
+    # Extract and Chunk
+    text = extract_content_from_pdf(file_bytes)
+    chunk = get_chunks(text)
+
+    # Get Embeddings
+    embedding = get_embeds(chunk)
+
+    # Store the document
+    supabase = get_supabase()
+    doc_id = str(uuid.uuid4())
+    supabase.table("documents").insert({
+        "id" : doc_id,
+        "user_id" : user_id,
+        "filename" : file.filename,
+        "chunk_count" : len(chunk)
+    }).execute()
+
+    # Store the vectors
+    rows = [{
+        "doc_id" : doc_id,
+        "user_id" : user_id,
+        "content" : chunk,
+        "embedding" : embedding,
+        "visibility" : "private"
+    } for chunk, embedding in zip(chunk, embedding)]
+    supabase.table("document_chunks").insert(rows).execute()
+
+    return {
+        "doc_id" : doc_id,
+        "chunks" : len(chunk)
+    }
