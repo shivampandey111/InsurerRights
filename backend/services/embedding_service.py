36689cbe6bb83a12a13a902f459eeca6d55dc4fb
shivampandey111 <shivampandey33033@gmail.com>
2026-06-04 12:16:03 +0530

    Embeddings

---
+++ backend/services/embedding_service.py
@@ -0,0 +1,28 @@
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from fastapi import HTTPException
+from dotenv import load_dotenv
+import os
+import time
+
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+
+def embedding_model():
+    return GoogleGenerativeAIEmbeddings(
+        model="gemini-embedding-2",
+        api_key=api_key
+    )
+
+def get_embeds(chunks:list[str]) -> list[list[float]]:
+    try:
+        model = embedding_model()
+        embeddings = []
+        batch_size = 20
+        for i in range(0, len(chunks), batch_size):
+            batch = chunks[i:i+batch_size]
+            embeddings.extend(model.embed_documents(batch))
+            time.sleep(1)
+        return embeddings
+    except Exception as e: # Printing the actual exception
+        print(e)
+        raise
