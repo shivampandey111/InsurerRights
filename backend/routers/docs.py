876a0478c1f76c0fa2e67dd735f8b2373467ffa5
shivampandey111 <shivampandey33033@gmail.com>
2026-06-17 04:17:55 +0530

    Delete API Added

--- backend/routers/docs.py
+++ backend/routers/docs.py
@@ -1,13 +1,13 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from backend.supaBase import get_supabase
 from backend.routers.dependencies import get_current_user
 
 router = APIRouter()
+supabase = get_supabase()
 
 @router.get('/api/documents')
 async def get_docs(
     user_Id: str = Depends(get_current_user)):
-    supabase = get_supabase()
     result = (
         supabase.table('documents')
         .select("filename", "id", "created_at")
@@ -16,3 +16,35 @@ async def get_docs(
         .execute()
     )
     return result.data
+
+@router.delete('/documents/delete/{doc_id}')
+async def del_docs(
+    doc_id = str,
+    user_Id : str = Depends(get_current_user)
+):
+    doc = (
+        supabase.table("documents")
+        .select("*")
+        .eq("id", doc_id)
+        .eq("user_id", user_Id)
+        .execute()
+    )
+    if not doc.data:
+        raise HTTPException(
+            status_code=404,
+            detail="Document Not Found"
+        )
+    # Deletion
+    response = (
+        supabase.table("documents")
+        .delete()
+        .eq("id", doc_id)
+        .execute()
+    )
+    return {
+        "response" : response.data,
+        "message" : "doc deleted",
+        "doc_id" : doc_id,
+        "doc" : doc.data[0]["filename"]
+    }
+    
