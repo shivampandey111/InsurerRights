cb2a4ab4c710a5bd37a8252e1ac58d40807dc7bf
shivampandey111 <shivampandey33033@gmail.com>
2026-06-26 21:04:27 +0530

    CORS Vercel

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app = FastAPI(title="Insurer Rights")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:5173"],
+    allow_origins = ["https://insurer-rights.vercel.app/"],
     allow_credentials = True,
     allow_headers = ['*'],
     allow_methods = ['*']
