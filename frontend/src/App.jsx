9bc2ab0cf412bf863385a8761ee409cbd3d46dda
shivampandey111 <shivampandey33033@gmail.com>
2026-06-26 09:44:37 +0530

    Included Global Chat and Primary Dashboard Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,30 @@
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import DashBoard from './pages/DashBoard.jsx'
 import Chat from './pages/Chat.jsx'
+import Landing from './pages/LandingPage.jsx'
+import GlobalChat from './pages/GlobalChat.jsx'
 import ProtectedRoute from './components/ProtectedRoute.jsx'
+import PrimaryDashBoard from './pages/PrimaryDashBoard.jsx'
 import Auth from './components/Auth.jsx'
+import { useState } from 'react'
+import Sidebar from './pages/SideBar.jsx'
 
 function App() {
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path = '/' element = {<Auth/>} />
+        <Route path = '/' element = {<Landing/>} />
         <Route path='/login' element = {<Auth/>} />
         <Route path='/dashboard' element = {
-          <ProtectedRoute><DashBoard/></ProtectedRoute>
-        } />
+          <ProtectedRoute>
+            <DashBoard/>
+          </ProtectedRoute>
+
+        }>
+          <Route index element={<PrimaryDashBoard/>} />
+          <Route path='assistant' element={<GlobalChat/>}/>
+        </Route>
         <Route path='chat/:doc_id' element = {
           <ProtectedRoute><Chat/></ProtectedRoute>
         } /> 
