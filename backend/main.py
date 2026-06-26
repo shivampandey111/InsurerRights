a708b915c1ec1ee642eb45cc739644b2a2d9c3d9
shivampandey111 <shivampandey33033@gmail.com>
2026-06-26 19:29:52 +0530

    Included Sentry SDK and Admin API Endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+import sentry_sdk, os
+from sentry_sdk.integrations.fastapi import FastApiIntegration
+from sentry_sdk.integrations.starlette import StarletteIntegration
+from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
-from backend.routers import auth, query, upload, docs, history, globalChat
+from backend.routers import auth, query, upload, docs, history, globalChat, admin_upload
+
+load_dotenv()
+
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+    integrations=[FastApiIntegration(), StarletteIntegration()],
+    traces_sample_rate=1.0
+)
 
 app = FastAPI(title="Insurer Rights")
 
@@ -37,6 +49,10 @@ app.include_router(
 app.include_router(
     upload.router,
     tags=['Upload']
+)
+app.include_router(
+    admin_upload.router,
+    tags=['Admin Upload']
 )
 
 @app.get('/api/health')
